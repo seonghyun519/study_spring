ec1694de39b922428697a85edbdb6f33a8525d0b
SEONGHYUN PARK <sparklep519@gmail.com>
2023-02-08 13:38:43 +0900

    http 규약 적용 동작 메서드명 제외

--- src/main/java/com/sparta/hanghaeboard/controller/BoardController.java
+++ src/main/java/com/sparta/hanghaeboard/controller/BoardController.java
@@ -38,13 +38,13 @@ public class BoardController {
         logger.info("메모 생성 api 컨트롤러 동작");
         return boardService.createBoard(boardRequestDTO);//데이터와 연결하는 서비스 부분을 개발/BoardService클래스에 createBoard 메서드 개발
     }
-    @PutMapping("/api/board/update/{id}") //수정 api
+    @PutMapping("/api/board/{id}") //수정 api
     public BoardResponseDTO updateMemo(@PathVariable Long id, @RequestBody BoardRequestDTO boardRequestDTO) {//@PathVariable값을 받아오고 수정 내용을 받기 위해 @RequestBody를 통해 Body부분의 데이터를 가져오고 username과 contents 두개뿐이니 앞서 만든 RequestDto를 그대로 사용
         logger.info("수정 컨트롤러 동작");
         logger.info("컨트롤러 수정 데이터" + String.valueOf(boardRequestDTO));
         return boardService.update(id, boardRequestDTO); // 보드서비스 클래스에 update 메서드에 id 값 필요하며 어떤값으로 변경되는지 알기 위해 requestDto 파라미터 두개(@RequestBody 바디 가져오기)
     }
-    @DeleteMapping("/api/board/delete/{id}/{pwd}") //ajax //$표시 ``백틱 없음
+    @DeleteMapping("/api/board/{id}/{pwd}") //ajax //$표시 ``백틱 없음
     public Long deleteMemo(@PathVariable Long id, @RequestBody BoardRequestDTO boardRequestDTO) {
         logger.info("삭제 컨트롤러 동작");
         logger.info("삭제 데이터" + String.valueOf(id));
